459ce9898ad035bf31015e7412a3e50d57ac14ec
NatalyaTseytlina <natashaceitlina@mail.ru>
2017-03-01 01:03:44 +0300

    subtask1 changed

--- src/main/java/ru/tn/courses/ntseytlina/task1/v2/subtask1/Main.java
+++ src/main/java/ru/tn/courses/ntseytlina/task1/v2/subtask1/Main.java
@@ -1,11 +1,42 @@
 package ru.tn.courses.ntseytlina.task1.v2.subtask1;
 
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
+    public static int [] array;
+    public static int [] zeroIndexes;
+    public static final int size = 15;
+    public static int zeroCounter;
+
     public static void main (String [] args) {
-        Array array = new Array(5);
-        array.fill();
-        System.out.println(array);
-        System.out.println(array.getIndexesOfZeroElements());
+        init();
+        findZeroIndexes();
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(zeroIndexes));
+    }
+
+    public static void init () {
+        array = new int[size];
+        zeroCounter = 0;
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(size);
+            if (array[i] == 0) {
+                zeroCounter++;
+            }
+        }
+    }
+
+    public static void findZeroIndexes () {
+        zeroIndexes = new int[zeroCounter];
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i] == 0) {
+                zeroIndexes[counter] = i;
+                counter++;
+            }
+        }
     }
 }
