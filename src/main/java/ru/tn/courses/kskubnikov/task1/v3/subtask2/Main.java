1b92bc5790573423656051032e29e2c9595ea6ba
lieandie <kirillli@gmail.com>
2017-03-07 01:29:45 +0300

    Кубики 3 вариант

--- src/main/java/ru/tn/courses/kskubnikov/task1/v3/subtask2/Main.java
+++ src/main/java/ru/tn/courses/kskubnikov/task1/v3/subtask2/Main.java
@@ -1,43 +1,58 @@
 package task1.v3.subtask2;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Random;
 
-/**
- * Created by Кирилл on 06.03.2017.
- */
 public class Main {
 
+    public static Random rnd = new Random();
+    public static int indexes[];
+
     public static void main(String[] args) {
-        boolean superHackerFlag = true;
-        int arr[] = cruchuVerchu(10, 2);
-        for (int i = 0; i < arr.length - 2; i++) {
-            int preSum = arr[i] + arr[i + 1] + arr[i + 2];
-            if (preSum >= 10) {
-                superHackerFlag = !superHackerFlag;
-                break;
-            }
-        }
-        if (superHackerFlag) {
-            System.out.println("KK U BROKE THIS");
-        } else {
-            System.out.println("NO,BRO.IT's IMPOSSIBLE");
-        }
+        int[] sequence = new int[3];
+        int[] arr = initArray(10, 2);
         System.out.println(Arrays.toString(arr));
+        if ((arr[indexes[0]] + arr[indexes[1]] > 9 || arr[indexes[0]] + arr[indexes[1]] < 4) || (indexes[0] % 3 == indexes[1] % 3)) {
+            System.out.println("NONONO BROH");
+        } else {
+            for (int i = 0; i < indexes.length; i++) {
+                switch (indexes[i] % 3) {
+                    case 0:
+                        sequence[0] = arr[indexes[i]];
+                        break;
+                    case 1:
+                        sequence[1] = arr[indexes[i]];
+                        break;
+                    case 2:
+                        sequence[2] = arr[indexes[i]];
+                        break;
+                }
+            }
+
+            if (sequence[0] == 0)
+                sequence[0] = 10 - sequence[1] - sequence[2];
+            else if (sequence[1] == 0)
+                sequence[1] = 10 - sequence[0] - sequence[2];
+            else sequence[2] = 10 - sequence[0] - sequence[1];
+            for(int i = 0; i < arr.length; i++){
+                arr[i] = sequence[i%3];
+            }
+            System.out.println("YAAAY u looking good");
+            System.out.println(Arrays.toString(arr));
+        }
+
     }
 
-    private static int[] cruchuVerchu(int size, int precountedDices) {
-        Random rnd = new Random();
-        int[] arr = new int[size];
-        LinkedList<Integer> used = new LinkedList<>();
-        for (int i = 0; i < precountedDices; i++) {
+    private static int[] initArray(int size, int initDices) {
+        int arr[] = new int[size];
+        indexes = new int[initDices];
+        for (int i = 0; i < initDices; i++) {
             Integer a;
             do {
-                a = rnd.nextInt(size);
+                a = rnd.nextInt(10);
             }
-            while (used.contains(a));
-            used.add(a);
+            while (Arrays.binarySearch(indexes, a) == 0);
+            indexes[i] = a;
             int arrElement;
             do {
                 arrElement = rnd.nextInt(7);
@@ -48,3 +63,4 @@ public class Main {
     }
 
 }
+
