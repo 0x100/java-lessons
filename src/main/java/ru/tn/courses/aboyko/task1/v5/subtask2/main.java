bc155474a38c4c261e8c66de6784922e66426ce2
Anatolii <insomniachedgehog@gmail.com>
2017-03-10 03:53:52 +0300

    main.java

---
+++ src/main/java/ru/tn/courses/aboyko/task1/v5/subtask2/main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public static void main(String[] args){
+
+        Scanner in = new Scanner (System.in);
+        System.out.println("Введите количество людей в очереди");
+        int peopleAmmount = in.nextInt();
+        int [] serviceTime = new int [peopleAmmount];
+
+        System.out.println("Укажите время ожидания для каждого клиента");
+          for (int i = 0; i < peopleAmmount; i++){
+            serviceTime[i] = in.nextInt();
+           }
+
+        System.out.println("Введите номер клиента в очереди");
+        int clientNumber = in.nextInt();
+        int waitingTime = 0;
+           for (int i = 0; i < clientNumber; i++){
+            waitingTime += serviceTime[i];
+           }
+        System.out.print("Время ожидания для клиента под номером " + clientNumber + " = " + waitingTime );
+
+    }
