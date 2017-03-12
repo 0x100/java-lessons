a3e47a25408d7d19c278a6fd65ba9d1205118461
krotis <krotis95@mail.ru>
2017-03-12 18:10:00 +0300

    Лабораторная 1

---
+++ src/main/java/ru/tn/courses/ptihomirov/task1/v1/subtask1/Main.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Павел on 12.03.2017.
+ */
+public class Main
+{
+    public static void main (String args[])
+    {
+        int n = 10; // кол-во элементов в массиве
+        int k = ((int) (Math.random() * 10)) + 1;
+        int Sum = 0;
+        int Arr[] = new int[n];
+
+        System.out.print("K = " + k + "\n");
+        for(int i = 0; i < n; i++) // инициализируем массив случайными натуральными числами
+        {
+            Arr[i] = ((int) (Math.random() * 100)) + 1;
+            System.out.print(Arr[i] + " ");
+        }
+        for(int i = 0; i < n; i++) // проверка элементов массива на кратность числу k
+        {
+            if(Arr[i] % k == 0)
+            {
+                Sum += Arr[i];
+            }
+        }
+        System.out.print("\n" + "Сумма = " + Sum);
+    }
+}
---
+++ src/main/java/ru/tn/courses/ptihomirov/task1/v1/subtask3/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+/**
+ * Created by Павел on 12.03.2017.
+ */
+public class Main2
+{
+    public static void main(String args[])
+    {
+        int n = 3;
+        int Arr_a[] = {1,2,3};
+        int Arr_b[] = {4,5,6};
+        int Arr_res[] = new int[n*2];
+
+        System.arraycopy(Arr_a,0, Arr_res, 0, Arr_a.length);
+        System.arraycopy(Arr_b,0, Arr_res, Arr_a.length, Arr_b.length);
+        Arrays.sort(Arr_res);
+
+        System.out.println(Arrays.toString(Arr_res));
+    }
+}
