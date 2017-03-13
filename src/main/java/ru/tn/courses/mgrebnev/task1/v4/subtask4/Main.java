012ab823f138e9144b00f4c78f17b8157babff70
Max Grebnev <solightingstats@yandex.ru>
2017-03-13 10:07:31 +0300

    Задача на подстроки (change)

--- src/main/java/ru/tn/courses/mgrebnev/task1/v4/subtask4/Main.java
+++ src/main/java/ru/tn/courses/mgrebnev/task1/v4/subtask4/Main.java
@@ -1,51 +1,68 @@
+package ru.tn.courses.mgrebnev.task1.v4.subtask4;
+
+import java.util.ArrayList;
+
 /**
- * Created by Max on 12.03.2017.
+ * Created by Max on 13.03.2017.
  */
-
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args){
 
-        String sentence = "домкубдомкубкуб";
+        String sentence = "домкубдомкубкубкуб";
 
-        ArrayList substring = new ArrayList();
+        ArrayList subStrings = new ArrayList();
 
-        int len_wrd = 2;
-        String sub = "";
+        int lengthSubWord = 2;
 
-        while(len_wrd <= sentence.length()){
-            for ( int i = 0; i <= sentence.length()-len_wrd; i++){
-
-                for ( int k = 0; k < len_wrd; k++){
-                    sub += sentence.charAt(i+k);
-                }
-                substring.add(sub);
-                sub = "";
+        while(lengthSubWord <= sentence.length()){
+            for ( int i = 0; i <= sentence.length()-lengthSubWord; i++){
+                subStrings.add(sentence.substring(i,i+lengthSubWord));
             }
-            len_wrd++;
+            lengthSubWord++;
         }
 
-        System.out.println("Count substings =  " + substring.size());
+        System.out.println("Кол-во подстрок =  " + subStrings.size());
 
         int count = 0;
-        int temp = 0;
-        String result = "";
+        int сountSubWord = 0;
 
-        for ( int i = 0; i < substring.size(); i++){
-            for (int j = 0; j < substring.size(); j++){
-                if (substring.get(i).equals(substring.get(j))){
+        ArrayList subWord = new ArrayList();
+        ArrayList counterSubWord = new ArrayList();
+
+        for ( int i = 0; i < subStrings.size(); i++){
+            for (int j = 0; j < subStrings.size(); j++){
+
+                if (subStrings.get(i).equals(subStrings.get(j))){
                     count++;
                 }
+
             }
-            if ( count >= temp ){
-                temp = count;
-                result = substring.get(i).toString();
+
+            if ( count >= сountSubWord){
+                сountSubWord = count;
+
+                if(!subWord.contains(subStrings.get(i).toString())){
+                    subWord.add(subStrings.get(i).toString());
+                    counterSubWord.add(count);
+                }
+
             }
             count = 0;
         }
 
-        System.out.println("\n" + "word = " + result + " count = " + temp);
+        int recommendedLength = 2;
+        String subStringsWithRerecommendedLength = "";
+
+        for(int i = 0; i < subWord.size(); i++){
+            System.out.println("Подстрока  " + subWord.get(i) + "  встречается " + counterSubWord.get(i) + " раз" );
+
+            if(subWord.get(i).toString().length() == recommendedLength){
+                subStringsWithRerecommendedLength +=subWord.get(i) + "  встречаются " + counterSubWord.get(i) + " раз \n";
+            }
+        }
+
+        System.out.println("\nПодстроки с длинной " + recommendedLength + ":\n \n" + subStringsWithRerecommendedLength);
+
     }
 }
