2f77cd1835bd79fd9ee44841a719ce0951b92235
NatalyaTseytlina <natashaceitlina@mail.ru>
2017-03-15 02:28:05 +0300

    Changed map to list

--- src/main/java/ru/tn/courses/ntseytlina/task1/v2/subtask4/Main.java
+++ src/main/java/ru/tn/courses/ntseytlina/task1/v2/subtask4/Main.java
@@ -1,9 +1,6 @@
 package ru.tn.courses.ntseytlina.task1.v2.subtask4;
 
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * На вход подается строка,
@@ -14,55 +11,67 @@ import java.util.Set;
 
 public class Main {
 
-    private static String string = "abyc" +
-            "utmr" +
+    private static String string = "utmr" +
             "utmr" +
             "abyc" +
             "utmr" +
+            "abyc" +
             "abyc";
 
     public static void main(String [] args) {
         System.out.println(findMostFrequentString());
     }
 
-    private static Map findMostFrequentString () {
-        Map<String, Integer> substrings = new LinkedHashMap<>();
-        Map<String, Integer> mostFrequentStrings = new LinkedHashMap<>();
+    private static List<List> findMostFrequentString () {
+        List<String> substringKeys = new ArrayList<>();
+        List<Integer> substringFrequency = new ArrayList<>();
+
+        List<List> mostFrequentStrings = new ArrayList<List>() {{
+            add(new ArrayList<String >());
+            add(new ArrayList<Integer>());
+        }};
 
         for (int i = 2; i < string.length(); i++) {
             for (int j = i; j < string.length() + 1; j++) {
                 String key = string.substring(j - i, j);
-                if (substrings.keySet().contains(key)) {
-                    substrings.replace(key, substrings.get(key) + 1);
+                if (substringKeys.contains(key)) {
+                    int index = substringKeys.indexOf(key);
+                    substringKeys.set(index, key);
+                    substringFrequency.set(index, substringFrequency.get(index) + 1);
                 } else {
-                    substrings.put(key, 1);
+                    substringKeys.add(key);
+                    substringFrequency.add(1);
                 }
             }
         }
 
         int numberOfSubstrings = 0;
         int length = 0;
-        for (String key : substrings.keySet()) {
-            if (substrings.get(key) > numberOfSubstrings) {
-                numberOfSubstrings = substrings.get(key);
-                mostFrequentStrings.clear();
-                mostFrequentStrings.put(key, substrings.get(key));
-                if (length < key.length()) {
-                    length = key.length();
-                }
-            } else if (substrings.get(key) == numberOfSubstrings) {
-                mostFrequentStrings.put(key, substrings.get(key));
+
+        for (String key : substringKeys) {
+            int index = substringKeys.indexOf(key);
+            if (substringFrequency.get(substringKeys.indexOf(key)) > numberOfSubstrings) {
+                numberOfSubstrings = substringFrequency.get(index);
+                mostFrequentStrings.get(0).clear();
+                mostFrequentStrings.get(1).clear();
+                mostFrequentStrings.get(0).add(key);
+                mostFrequentStrings.get(1).add(substringFrequency.get(index));
+                length = key.length();
+            } else if (substringFrequency.get(substringKeys.indexOf(key)) == numberOfSubstrings) {
+                mostFrequentStrings.get(0).add(key);
+                mostFrequentStrings.get(1).add(substringFrequency.get(index));
                 if (length < key.length()) {
                     length = key.length();
                 }
             }
         }
 
-        Set<String> keys = new HashSet<>(mostFrequentStrings.keySet());
+        List<String> keys = new ArrayList<>(mostFrequentStrings.get(0));
 
         for (String key : keys) {
             if (key.length() < length) {
-                mostFrequentStrings.remove(key);
+                mostFrequentStrings.get(1).remove(mostFrequentStrings.get(0).indexOf(key));
+                mostFrequentStrings.get(0).remove(key);
             }
         }
 
