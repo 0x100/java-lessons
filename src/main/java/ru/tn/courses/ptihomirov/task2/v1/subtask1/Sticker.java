1be23b41b6394127eeee9ce3ac7887f8e22f9d69
krotis <krotis95@mail.ru>
2017-04-03 01:29:23 +0300

    Лабораторная №2

---
+++ src/main/java/ru/tn/courses/ptihomirov/task2/v1/subtask1/Sticker.java
@@ -0,0 +1,20 @@
+package ru.tn.courses.ptihomirov.task2.v1.subtask1;
+/**
+ * Created by Павел on 02.04.2017.
+ */
+public class Sticker
+{
+    // Координаты объявления
+    public int left;
+    public int top;
+    public int right;
+    public int bottom;
+
+    public Sticker(int left, int top, int right, int bottom)
+    {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+}
