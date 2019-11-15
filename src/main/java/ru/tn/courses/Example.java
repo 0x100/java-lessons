3287437da664c1c34c5c960ed0341487b44e73d9
0x100 <lysenko.ilya@gmail.com>
2019-11-15 03:57:27 +0300

    Updated example

--- src/main/java/ru/tn/courses/Example.java
+++ src/main/java/ru/tn/courses/Example.java
@@ -1,7 +1,11 @@
 package ru.tn.courses;
 
+import java.util.Scanner; 
+
 public class Example {
     public static void main(String[] args) {
-        System.out.println("lol");
+        System.out.println("What's your name?");
+        String name = new Scanner(System.in).nextLine();        
+        System.out.println("Hello, " + name);
     }
 }
